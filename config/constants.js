30e8990410bd27aa9d00e955d046a30db073aaca
Mangesh Pralhad Pandit <86043310+alicehack2020@users.noreply.github.com>
2023-04-06 16:01:26 +0530

    vercel

--- config/constants.js
+++ config/constants.js
@@ -1,10 +1,8 @@
 const urlInfo = {
     CLIENT_URL: "https://alleventfront-sample1.vercel.app/",
-    SERVER_URL: "https://all-event-back-end.vercel.app/",
-    SERVER_URL_ISSUE: "https://all-event-back-end.vercel.app//",
-    Login_URL_ISSUE: "https://all-event-back-end.vercel.app/auth/login/success",
-    CLINE_URL_ISSUE:"htps://alleventfront-sample1.vercel.app"
-
-    
+    SERVER_URL: "https://alleventbackendupdated.onrender.com/",
+    SERVER_URL_ISSUE: "https://alleventbackendupdated.onrender.com//",
+    Login_URL_ISSUE: "https://alleventbackendupdated.onrender.com/auth/login/success",
+    CLINE_URL_ISSUE:"htps://alleventfront-sample1.vercel.app"  
 }
 module.exports = urlInfo;
