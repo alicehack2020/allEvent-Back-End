276a56269fff7a03b8c62e4f29ac4ebfe09edb71
Mangesh Pralhad Pandit <86043310+alicehack2020@users.noreply.github.com>
2023-04-06 18:12:04 +0530

    redirect url issue

--- passport.js
+++ passport.js
@@ -1,15 +1,19 @@
-var GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
 const passport = require("passport");
-require('dotenv').config()
+const GoogleStrategy = require("passport-google-oauth2").Strategy;
+require("dotenv").config();
+
+// Configure Google OAuth 2.0 strategy
 passport.use(
 	new GoogleStrategy(
 		{
 			clientID: process.env.CLIENT_ID,
 			clientSecret: process.env.CLIENT_SECRET,
-			callbackURL: "/auth/google/callback",
+			callbackURL: "https://alleventbackendupdated.onrender.com/auth/google/callback", // Update with your HTTPS redirect URI
 			scope: ["profile", "email"],
 		},
 		function (accessToken, refreshToken, profile, callback) {
+			// Handle authentication logic here
+			// ...
 			callback(null, profile);
 		}
 	)
